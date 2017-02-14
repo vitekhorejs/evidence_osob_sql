ca0f586a2f6d438c347c0e37fee6cb8927caf975
horejvi14 <horejvi14@DST20716>
2017-02-14 09:12:23 +0100

    tr

---
+++ evidence_osob_sql/TodoItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace evidence_osob_sql
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Text { get; set; }
+        public int Year { get; set; }
+        public TodoItem()
+        {
+        }
+        public override string ToString()
+        {
+            return "ID" + ID + " Name " + Name + " SurName " + SurName + " Text " + Text + " Year " + Year;
+        }
+    }
+}
